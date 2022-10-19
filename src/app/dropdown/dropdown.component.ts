828056fbac60f1fa404b057c5e6dd6ff5079a051
Lukasz Staniszewski <l.staniszewski01@gmail.com>
2022-10-19 22:52:56 +0200

    added basic styles to card and list

--- src/app/dropdown/dropdown.component.ts
+++ src/app/dropdown/dropdown.component.ts
@@ -6,6 +6,7 @@ import {
   ViewChild,
   ElementRef,
   AfterViewInit,
+  HostListener,
 } from '@angular/core';
 import { DogBreeds } from '../home-page/home-page.component';
 
@@ -18,16 +19,14 @@ export class DropdownComponent implements AfterViewInit {
   selectedBreed = 'wybierz rasę';
   @Input() dogBreeds: DogBreeds | undefined;
   @Output() selectedOption = new EventEmitter<string>();
-  @ViewChild('selected') selected?: ElementRef<HTMLSelectElement>;
 
   ngAfterViewInit(): void {
-    const test = sessionStorage.getItem('breed');
-    if (!test) return;
-    this.selectedBreed = test;
+    const breed = sessionStorage.getItem('breed');
+    if (!breed) return;
+    this.selectedBreed = breed;
   }
 
   setSelectedOption(ref: HTMLSelectElement) {
-    console.log(ref.value);
     sessionStorage.setItem('breed', ref.value);
 
     this.selectedOption.emit(ref.value);
