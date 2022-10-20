d4fdc42d22fa19e48dd1afcd905e226f6a777d08
Lukasz Staniszewski <l.staniszewski01@gmail.com>
2022-10-20 23:12:33 +0200

    button not showing as disabled bug fix

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -14,6 +14,7 @@ export class CardComponent implements OnChanges {
 
   ngOnChanges(): void {
     const adjustedBreedName = this.adjustBreedName();
+    if (!adjustedBreedName) return;
     this.wikiLink = `https://en.wikipedia.org/wiki/${adjustedBreedName}`;
   }
 
